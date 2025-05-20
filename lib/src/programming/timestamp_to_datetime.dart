b40dfe9b552012c793fd29e670b19dc64f05508f
Filip Hracek <filip.hracek@gmail.com>
2025-05-20 18:10:17 +0200

    Add +10ms to output

--- lib/src/programming/timestamp_to_datetime.dart
+++ lib/src/programming/timestamp_to_datetime.dart
@@ -10,15 +10,35 @@ String rewriteTimestamps(String full) {
   final lines = full.trim().split(delimiter);
 
   final buf = StringBuffer();
+  DateTime? previous;
+
+  String format(DateTime dateTime) {
+    final buf = StringBuffer();
+    buf.write(dateTime.toIso8601String());
+    if (previous != null) {
+      final duration = dateTime.difference(previous!);
+      final ms = duration.inMilliseconds;
+      buf.write('\t+');
+      buf.write(ms.toString());
+      buf.write('ms');
+    }
+    return buf.toString();
+  }
 
   for (final line in lines) {
+    bool lineHasTimestamp = false;
     final out = line.replaceAllMapped(_thirteenDigitRegExp, (match) {
+      lineHasTimestamp = true;
       final milliseconds = int.parse(match.group(0)!);
 
       final dateTime = DateTime.fromMillisecondsSinceEpoch(milliseconds);
-      return dateTime.toIso8601String();
+      final formatted = format(dateTime);
+      previous = dateTime;
+      return formatted;
     });
-    buf.writeln(out);
+    if (lineHasTimestamp) {
+      buf.writeln(out);
+    }
   }
 
   return buf.toString();
